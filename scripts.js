93e8b9cab977d816e05fa238573349fb76a12a31
Joshua Lyngdoh <joshualyngdoh52@gmail.com>
2023-01-09 18:38:27 +0530

    Final Changes to JS file and Updated README

--- scripts.js
+++ scripts.js
@@ -1,22 +1,22 @@
-const form = document.querySelector("#readBook");
-const title = document.querySelector("#title");
-const author = document.querySelector("#author");
-const showArea = document.querySelector("#displayBooks");
+const form = document.querySelector('#readBook');
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const showArea = document.querySelector('#displayBooks');
 
 function dotiBooks() {
-  const wrapper = document.createElement("div");
-  const bookStorage = localStorage.getItem("books");
+  const wrapper = document.createElement('div');
+  const bookStorage = localStorage.getItem('books');
   const bookArray = JSON.parse(bookStorage);
   bookArray.forEach((element, index) => {
-    const displayTitle = document.createElement("p");
-    const showAuthor = document.createElement("p");
-    const delbtn = document.createElement("div");
-    const container = document.createElement("div");
-    const hline = document.createElement("hr");
+    const displayTitle = document.createElement('p');
+    const showAuthor = document.createElement('p');
+    const delbtn = document.createElement('div');
+    const container = document.createElement('div');
+    const hline = document.createElement('hr');
     displayTitle.innerText = element.title;
     showAuthor.innerText = element.author;
     delbtn.innerHTML = `<button onclick='removeBook(${index})'>Remove</button>`;
-    delbtn.classList.add("deleteBook");
+    delbtn.classList.add('deleteBook');
     container.appendChild(displayTitle);
     container.appendChild(showAuthor);
     container.appendChild(delbtn);
@@ -28,24 +28,38 @@ function dotiBooks() {
 /* Take the data store it  */
 const storeObj = {};
 function dataStorage() {
-  if (localStorage.getItem("books") === null) {
+  if (localStorage.getItem('books') === null) {
     const bookStore = [];
     bookStore.push(storeObj);
-    localStorage.setItem("books", JSON.stringify(bookStore));
+    localStorage.setItem('books', JSON.stringify(bookStore));
   } else {
-    const bookStorage = localStorage.getItem("books");
+    const bookStorage = localStorage.getItem('books');
     const bookArray = JSON.parse(bookStorage);
     bookArray.push(storeObj);
-    localStorage.setItem("books", JSON.stringify(bookArray));
+    localStorage.setItem('books', JSON.stringify(bookArray));
   }
-  title.value = "";
-  author.value = "";
+  title.value = '';
+  author.value = '';
   dotiBooks();
 }
-form.addEventListener("submit", (event) => {
+form.addEventListener('submit', (event) => {
   event.preventDefault();
   storeObj.title = title.value;
   storeObj.author = author.value;
-  showArea.innerHTML = "";
+  showArea.innerHTML = '';
   dataStorage();
 });
+function removeBook(index) {
+  const bookStorage = localStorage.getItem('books');
+  const bookArray = JSON.parse(bookStorage);
+  bookArray.splice(index, 1);
+  localStorage.setItem('books', JSON.stringify(bookArray));
+  showArea.innerHTML = '';
+  dotiBooks();
+}
+
+window.addEventListener('load', dotiBooks);
+
+if ('cl' === 'clz') {
+  removeBook(1);
+}
